4d0dd5e5dc60127666cb03a0c524fa64865962c2
Marc <marc.berli@windowslive.com>
2018-12-10 21:07:21 +0100

    Use ClientForm in ClientRouter

--- src/client/components/ClientRouter.js
+++ src/client/components/ClientRouter.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import ClientList from './ClientList';
+import Client from './Client'
 
-const newClient = () => 'Neuer Kunde anlegen'
-const ClientDetail = () => 'Das ist dein Kunde'
 const ClientRouter = () => {
   return (
     <>
       <Switch>
         <Route path="/clients" exact component={ClientList} />
-        <Route path="/clients/new" component={newClient} />
-        <Route path="/clients/:clientId" component={ClientDetail} />
+        <Route path="/clients/new" component={Client} />
+        <Route path="/clients/:clientId" component={Client} />
       </Switch>
     </>
   )
