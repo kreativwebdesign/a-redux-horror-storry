d5e1a3c25b477503efed6d051e33581c78bc7780
Simon Stucki <simon_stucki@hotmail.com>
2018-12-10 20:14:16 +0100

    Apply formatter

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import ClientRouter from 'src/client/components/clientRouter';
-import Navbar from 'src/routing/Navbar';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import ClientRouter from 'src/client/components/clientRouter'
+import Navbar from 'src/routing/Navbar'
 import AppContainer from './AppContainer'
-import styles from './styles/app.scss';
+import styles from './styles/app.scss'
 import './styles/root.scss'
 
-const Index = () => <h2>Home</h2>;
+const Index = () => <h2>Home</h2>
 
 const App = () => (
   <AppContainer>
     <main className={styles.app}>
       <Navbar />
       <section className={styles.pageBody}>
-      <Switch>
-        <Route path="/" exact component={Index} />
-        <Route path="/clients" component={ClientRouter} />
-      </Switch>
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/clients" component={ClientRouter} />
+        </Switch>
       </section>
     </main>
   </AppContainer>
-);
+)
 
-export default App;
+export default App
