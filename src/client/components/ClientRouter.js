cd0899f33caebdb41b3533093ed00a523c973bc5
Simon Stucki <simon_stucki@hotmail.com>
2018-12-10 20:01:23 +0100

    Remove obsolete links

--- src/client/components/ClientRouter.js
+++ src/client/components/ClientRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import ClientList from './ClientList';
 
 const newClient = () => 'Neuer Kunde anlegen'
@@ -7,9 +7,6 @@ const ClientDetail = () => 'Das ist dein Kunde'
 const ClientRouter = () => {
   return (
     <>
-      <Link to="/clients/new">Neuer Kunde</Link>
-      <Link to="/clients/">Alle Kunden</Link>
-      <Link to="/clients/4321">Detail Kunde</Link>
       <Switch>
         <Route path="/clients" exact component={ClientList} />
         <Route path="/clients/new" component={newClient} />
