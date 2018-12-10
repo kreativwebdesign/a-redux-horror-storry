4371f56057a8c242fd69170fa7e09b905543ffed
Marc <marc.berli@windowslive.com>
2018-12-10 21:04:40 +0100

    Create Add types to client domain

--- src/client/redux/types.js
+++ src/client/redux/types.js
@@ -1,13 +1,18 @@
 import { pending, succeeded, failed } from "src/redux/helper/statusHelper";
-import { fetch } from 'src/redux/helper/operationHelper'
+import { fetch, add } from 'src/redux/helper/operationHelper'
 
 
 const NAMESPACE = 'CLIENT'
 const FETCH_CLIENT = fetch(NAMESPACE)
+const ADD_CLIENT = add(NAMESPACE)
 
 export default {
   FETCH_CLIENT,
   FETCH_CLIENT_PENDING: pending(FETCH_CLIENT),
   FETCH_CLIENT_SUCCEEDED: succeeded(FETCH_CLIENT),
   FETCH_CLIENT_FAILED: failed(FETCH_CLIENT),
+  ADD_CLIENT,
+  ADD_CLIENT_PENDING: pending(ADD_CLIENT),
+  ADD_CLIENT_SUCCEEDED: succeeded(ADD_CLIENT),
+  ADD_CLIENT_FAILED: failed(ADD_CLIENT),
 }
