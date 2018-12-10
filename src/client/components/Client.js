b3eebd9dcd854f2cfcfe813d160991ff61a01503
Marc <marc.berli@windowslive.com>
2018-12-10 21:07:03 +0100

    Create Client Form

---
+++ src/client/components/Client.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { Formik, Field, ErrorMessage } from 'formik'
+import { selectors, types } from '../redux'
+
+const defaultClient = {
+  id: undefined,
+  lastName: undefined,
+  firstName: undefined,
+  emailAddress: undefined,
+}
+
+const Client = ({ client, fetchClient, addClient, match }) => {
+  const { clientId } = match.params
+  if (clientId && !client) {
+    fetchClient()
+    return 'Fetching client'
+  }
+
+  return (
+    <Formik
+      initialValues={client || defaultClient}
+      onSubmit={(values) => {
+        addClient(values)
+      }}
+    >
+      {({
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Field name="firstName" type="text"/>
+          <ErrorMessage name="firstName" component="div" />
+          <Field name="lastName" type="text"/>
+          <ErrorMessage name="lastName" component="div" />
+          <Field name="emailAddress" type="text"/>
+          <ErrorMessage name="emailAddress" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  )
+}
+
+const mapStateToProps = (state, props) => {
+  const { clientId } = props.match.params
+  return {
+    client: clientId ? selectors.selectDataById(clientId)(state) : undefined
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchClient: () => dispatch({ type: types.FETCH_CLIENT }),
+  addClient: client => dispatch({ type: types.ADD_CLIENT, payload: client })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Client)
