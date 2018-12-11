996fcaf5260ab88207eddfd165eb4011db92c1a6
Marc <marc.berli@windowslive.com>
2018-12-11 11:20:01 +0100

    Refactor sagaHelper

--- src/redux/helper/sagaHelper.js
+++ src/redux/helper/sagaHelper.js
@@ -1,32 +1,39 @@
 import { put, takeEvery, call } from 'redux-saga/effects'
 import { setFetchOperation, setAddOperation } from './addOperationHelper'
 
-export const createFetchSaga = ({ apiCall, types }) => function* fetchSaga() {
-  yield put({ type: types.FETCH.PENDING })
-  const response = yield call(apiCall);
-  const payload = setFetchOperation(response);
-  if (response.meta.status.error) {
-    yield put({ type: types.FETCH.FAILED, payload })
-  } else {
-    yield put({ type: types.FETCH.SUCCESS, payload })
-  }
+const createWatch = ({ type, saga }) => function* watchSaga() {
+  yield takeEvery(type, saga)
 }
 
-export const createWatchFetch = ({ types, fetchSaga }) => function* watchFetchSaga() {
-  yield takeEvery(types.FETCH.DO, fetchSaga)
-}
-
-export const createAddSaga = ({ apiCall, types }) => function* addSaga(data) {
-  yield put({ type: types.ADD.PENDING })
+const createSaga = ({ apiCall, types, setOperation }) => function* saga(data = {}) {
+  yield put({ type: types.PENDING })
   const response = yield call(() => apiCall(data.payload));
-  const payload = setAddOperation(response);
+  const payload = setOperation(response);
   if (response.meta.status.error) {
-    yield put({ type: types.ADD.FAILED, payload })
+    yield put({ type: types.FAILED, payload })
   } else {
-    yield put({ type: types.ADD.SUCCESS, payload })
+    yield put({ type: types.SUCCESS, payload })
   }
 }
 
-export const createWatchAdd = ({ types, addSaga }) => function* watchFetchSaga() {
-  yield takeEvery(types.ADD.DO, addSaga)
-}
+export const createFetchSaga = ({ apiCall, types }) => createSaga({
+  types: types.FETCH,
+  apiCall,
+  setOperation: setFetchOperation
+})
+
+export const createAddSaga = ({ apiCall, types }) => createSaga({
+  types: types.ADD,
+  apiCall,
+  setOperation: setAddOperation
+})
+
+export const createWatchFetch = ({ types, fetchSaga }) => createWatch({
+  type: types.FETCH.DO,
+  saga: fetchSaga
+})
+
+export const createWatchAdd =  ({ types, addSaga }) => createWatch({
+  type: types.ADD.DO,
+  saga: addSaga
+})
