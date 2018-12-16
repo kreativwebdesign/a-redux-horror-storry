e697f3dbc62bff5b367fdd02648e071d3615470b
Marc <marc.berli@windowslive.com>
2018-12-16 18:09:07 +0100

    Remove old comments

--- src/datalayer/client/controllers/fetch-single-client/sagas/index.js
+++ src/datalayer/client/controllers/fetch-single-client/sagas/index.js
@@ -2,7 +2,6 @@ import { put, select, call, takeEvery } from 'redux-saga/effects'
 import { selectors } from '../index'
 import { timetableSelectors, types } from 'src/datalayer/client'
 import * as api from 'src/api'
-// a saga describing how to fetch a single client
 
 function* fetchSingleClient({ payload: { clientId } }) {
   const resourceAvailable = yield select(
--- src/datalayer/client/controllers/fetch-single-client/selectors/index.js
+++ src/datalayer/client/controllers/fetch-single-client/selectors/index.js
@@ -1,4 +1,3 @@
-// selector selecting the status of the controllers operation from state
 import { createControllerSelectors } from 'src/datalayer/helper/selector-helper'
 import { NAMESPACE } from 'src/datalayer/client/namespace'
 import { NAMESPACE as CONTROLLER_NAME } from '../namespace'
