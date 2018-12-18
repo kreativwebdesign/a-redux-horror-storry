3ee1ae41ac143f7e359c342c699e9f4da589e358
Simon Stucki <simon_stucki@hotmail.com>
2018-12-18 09:46:54 +0100

    Redirect home site to /clients

--- src/view/app/App.js
+++ src/view/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { ClientRouter } from 'src/view/client'
 import { BookingRouter } from 'src/view/booking'
 import NavigationBar from 'src/routing/components/navigation-bar'
@@ -7,7 +7,7 @@ import AppContainer from './AppContainer'
 import styles from './styles/app.scss'
 import './styles/root.scss'
 
-const Index = () => <h2>Home</h2>
+const Index = () => <Redirect to="/clients" />
 
 const App = () => (
   <AppContainer>
